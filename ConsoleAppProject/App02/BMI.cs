2c85584cee686e2386046efc0bed3a36255f9141
hsadiq32 <20135112s@gmail.com>
2021-02-18 22:22:52 +0000

    added bame messages and improved console input

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -12,6 +12,8 @@ namespace ConsoleAppProject
     {
         SyntaxGenerator syntaxGen = new SyntaxGenerator();
         ArrayList data = new ArrayList();
+        string bameMessage1 = "If you are Black, Asian or other minority ethnic groups you have a higher risk";
+        string bameMessage2 = "Adults 23.0 or more are at increased risk Adults 27.5 or more are at high risk";
 
         public void Run()
         {
@@ -32,15 +34,19 @@ namespace ConsoleAppProject
             double weight = 0;
             if (option == 1)
             {
-                height = reader.DoubleInputChecker("Metres:");
-                weight = reader.DoubleInputChecker("KG:");
+                Console.WriteLine(syntaxGen.SyntaxFiller1("Enter Height:"));
+                height = reader.DoubleInputChecker(" Metres:");
+                Console.WriteLine(syntaxGen.SyntaxFiller1("Enter Weight:"));
+                weight = reader.DoubleInputChecker(" KG:");
                 unitVal = 0;
             }
             else if (option == 2)
             {
-                int foot = reader.IntInputChecker("Feet:");
-                double inches = reader.DoubleInputChecker("Inches:");
-                weight = reader.DoubleInputChecker("Pounds:");
+                Console.WriteLine(syntaxGen.SyntaxFiller1("Enter Height:"));
+                int foot = reader.IntInputChecker(" Feet:");
+                double inches = reader.DoubleInputChecker(" Inches:");
+                Console.WriteLine(syntaxGen.SyntaxFiller1("Enter Weight:"));
+                weight = reader.DoubleInputChecker(" Pounds:");
                 height = (foot * 12) + inches;
                 unitVal = 1;
             }
@@ -61,6 +67,8 @@ namespace ConsoleAppProject
                 if (bmi <= bmiRange) { break; }
             }
             Console.WriteLine(syntaxGen.SyntaxFiller1("Your BMI is: " + bmi.ToString("0.#") + " this is " + description));
+            Console.WriteLine(syntaxGen.SyntaxFiller1(bameMessage1));
+            Console.WriteLine(syntaxGen.SyntaxFiller1(bameMessage2));
             syntaxGen.SyntaxFiller2();
 
         }
